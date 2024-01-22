e47c1ae342f3f02994f6ddb23a8ee0d88ecff3d0
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-01-22 21:00:23 +0530

    feat: add sans and mono font add fontFamily in tailwind.config.ts add sans and mono font in tailwind config add cn function in layout body tag to render fontSans and fontMono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const fontMono = Space_Grotesk({ subsets: ['latin'], variable: '--font-mono' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          'font-sans antialiased',
+          fontSans.variable,
+          fontMono.variable
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
