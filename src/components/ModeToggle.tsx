2d7b3161bf32b6dc5991747f55ed2630ebdcb837
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-01-22 21:42:34 +0530

    feat: add ModeToggle to change themes
    add shadcn/ui button component
    add shadcn/ui dropdown menu component
    add next-themes
    add theme-provider
    add ModeToggle to toggle between dark and light mode

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import * as React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <div className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:hidden flex gap-1">
+            Light Mode
+            <Sun className="h-[1.2rem] w-[1.2rem]" />
+          </div>
+          <div className="rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 hidden dark:flex gap-1">
+            Dark Mode
+            <Moon className="h-[1.2rem] w-[1.2rem]" />
+          </div>
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
