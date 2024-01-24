459b9d9e42c82905a6e48c1a78ab2c3f576cf785
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-01-25 01:07:57 +0530

    feat: change font-mono to font-serif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="mt-36">
+      <p className="text-4xl text-center font-semibold">
+        The <span className="font-serif italic">only</span> NextJS starter kit
+      </p>
+      <p className="text-4xl text-center font-semibold">
+        you&apos;ll <span className="font-serif italic">ever</span> need.
+      </p>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,7 +75,7 @@ const config = {
       },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
-        mono: ['var(--font-mono)', ...fontFamily.mono],
+        serif: ['var(--font-serif)', ...fontFamily.serif],
       },
     },
   },
